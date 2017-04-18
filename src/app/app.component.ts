152068ef3db2362da52e36ae7fe753992dd3bf42
Eric BREHAULT <ebrehault@gmail.com>
2017-04-18 18:23:34 +0200

    enable plone traversing and add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Traverser } from 'angular-traversal';
+import { PloneViews } from '@plone/restapi-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(
+    private views: PloneViews,
+    private traverser: Traverser,
+  ) {
+    this.views.initialize();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RESTAPIModule } from '@plone/restapi-angular';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +13,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RESTAPIModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'CONFIGURATION', useValue: {
+        BACKEND_URL: 'http://localhost:8080/Plone',
+      }
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
